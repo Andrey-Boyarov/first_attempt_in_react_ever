d1f011797b46c102c35b9290665e4f58237cdf5e
Andrey <boyaroff2014@gmail.com>
2022-02-05 22:37:15 +0400

    First day, simple elements, more js-like html than actually react things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Hello</h1>
+        <p className="text">HelloText</p>
+        <hr/>
+        <select>
+            <option value={1}>Pizza</option>
+            <option value={2}>Sushi</option>
+            <option value={3}>{React.version}</option>
+        </select>
+        <hr/>
+        <img src={require("./react-logo.png")} alt={"react"} title={"react"}/>
+      </div>
   );
 }
 
